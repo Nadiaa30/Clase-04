930e03c43e45084f78d89e34fc1e7756fa9c426a
Nadia Sanchez <nadiiasanchez11@gmail>
2022-09-08 21:45:29 -0300

    Agregue html,css,js

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Clase-04
+** ejercicio
+ *Crear objetos(usando clases)de los siguientes componentes:
+ -video(fuente,)
+ -audio
+ -boton(color,tamaño)
+ -lista de elementos
+
+
---
+++ js/app.js
@@ -0,0 +1,34 @@
+let myDom = document;
+
+const myDog = {
+    "name":"Firulais",
+    "age" : 5,
+    "legs" : 4,
+};
+
+const myCat = {
+    name:"Michi",
+    age : 5,
+    legs : 4,
+};
+
+console.log(myCat.name);
+console.log(myDog.legs);
+
+/*function Mouse(){
+   this.age = 4;
+   this.name = "Perez"; 
+}*/
+
+class Mouse{
+    constructor(name){
+       this.name = name;
+    }
+}
+this.document
+const myMouse = new Mouse();
+console.log(myMouse);
+
+
+
+
